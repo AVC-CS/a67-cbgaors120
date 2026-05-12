b8ae290f2f0aa62b676280730fa9a61d25a63f4f
cbgaors120 <bgaor@students.avc.edu>
2026-05-11 22:57:34 -0700

    First compile test.

--- main.hpp
+++ main.hpp
@@ -5,5 +5,14 @@ int hanoi(int n, char from, char to, char aux);
 
 int hanoi(int n, char from, char to, char aux)
 {
+    static int count = 0;
+    if (n == 1){
+        count ++;
+        return count;
+    }
+    hanoi(n-1, from, aux, to);
+    count++;
+    hanoi(n-1, aux, to, from);
+    return count;
     // TODO
 }
